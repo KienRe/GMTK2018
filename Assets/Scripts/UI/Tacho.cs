e44c012bb93cb143e6aad149b4a6395e8d83e68e
Andreas Zechmeister <andreaszechmeister@chello.at>
2018-09-01 12:51:23 +0200

    UI speed text nofric Physics mat scripts sorted

--- Assets/Scripts/UI/Tacho.cs
+++ Assets/Scripts/UI/Tacho.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tacho : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class Tacho : MonoBehaviour
     public Vector2 tachoGrenze = new Vector2(0, 180);
     public Vector2 speedLimit = new Vector2(0, 100);
     public Transform tachoNadel;
+    public Text speedText;
 
     void Update()
     {
         float mappedValue = Remapper.Remap(speed, speedLimit.x, speedLimit.y, tachoGrenze.x, tachoGrenze.y);
         tachoNadel.localRotation = Quaternion.Euler(0f, 0f, -mappedValue);
+        speedText.text = speed.ToString("0");
     }
 }
