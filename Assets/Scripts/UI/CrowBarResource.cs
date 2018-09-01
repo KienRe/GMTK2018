e67df84eef3fd1d2e9a082019c131eea7a36261d
Andreas Zechmeister <andreaszechmeister@chello.at>
2018-09-01 14:50:33 +0200

    Crowbar linked

---
+++ Assets/Scripts/UI/CrowBarResource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrowBarResource : MonoBehaviour {
+
+    public Image crowbar;
+    public PlayerController player;
+	
+	void Update ()
+    {
+        crowbar.fillAmount = player.metalBarRessource;
+	}
+}
