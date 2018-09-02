e5d48ebd0f5adf99163d1ff8ba763efe19a3621a
Kienauer Rene <rene102@live.de>
2018-09-02 11:36:50 +0200

    Added Sparks VFX on Left/Right Side Collision

---
+++ Assets/Scripts/Player/PlayerVFX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVFX : MonoBehaviour
+{
+    [Header("Scene Assignmnets")]
+    public ParticleSystem leftSideSparks;
+    public ParticleSystem rightSideSparks;
+
+    public bool IsLeftSideCol { get; set; }
+    public bool IsRightSideCol { get; set; }
+
+
+    private void Update()
+    {
+        if (IsLeftSideCol)
+            leftSideSparks.Play();
+
+        if (IsRightSideCol)
+            rightSideSparks.Play();
+    }
+}
